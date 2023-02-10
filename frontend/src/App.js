ec7d49f30edca5423aeea28fef2fccc472088da4
Sayan97Ghosh <sayanghosh97x@gmail.com>
2023-02-10 22:38:27 +0530

    day-1

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import Header from "./Pages/Header/Header";
+
+import "./App.css"
+// import Sidebar from "./Pages/Sidebar/Sidebar";
+import Widgets from "./Pages/Widgets/Widgets";
+import Footer from "./Pages/Footer/Footer";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Widgets/>
+      <Footer/>
+     {/* <Sidebar/> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Pages/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import "./Footer.css";
+import HeaderOption from "../Header/HeaderOption";
+
+function Footer() {
+    
+  return (
+    <div className="footer" style={{ visibility: "hidden" }}>
+      <div className="footer__options">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
